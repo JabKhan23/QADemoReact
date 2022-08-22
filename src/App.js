19874b6ebd19ddea42fc42579d13c6aa6f678330
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-08-22 16:36:06 +0100

    added my own elements to the demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import logo from './flying-lm.gif';
 import './App.css';
 
-function App() {
+const AboutMe = () => {
+
+  const address = {
+      firstname: "Jab",
+      surname: "Khan",
+      line1: "123",
+      line2: "Fake Street",
+      city: "Manchester",
+      postcode: "M12 3AB"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={logo} className="App-logo" alt="logo" />
+        <h1>Firstname: {address.firstname}</h1>
+        <h1>Surname: {address.surname}</h1>
+        <h1>Address: {address.line1}</h1>
+        <h1>{address.line2}</h1>
+        <h1>{address.city}</h1>
+        <h1>{address.postcode}</h1>
+        </header>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/JabKhan23/QADemoReact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Here is my GitHub
+          </a>
     </div>
-  );
+);
 }
 
-export default App;
+
+export default AboutMe;

a549969f941d659b6db2ee5553e8e722b8f89b01
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-08-23 14:04:48 +0100

    new tutorial - components

--- src/App2.js
+++ src/App2.js
@@ -5,12 +5,11 @@ import ReactDOM from 'react-dom/client';
 import './header.jsx';
 import Header from './header.jsx';
 import Hello from './Hello.jsx';
-import AboutMe from './Hello.jsx';
 
 
  const App = () => {
   return (
-    <AboutMe />
+    <Hello />
    );
  }
 
---
+++ src/Hello.jsx
@@ -0,0 +1,39 @@
+import logo from './flying-lm.gif';
+
+const AboutMe = () => {
+
+    const address = {
+       firstname: "Jab",
+       surname: "Khan",
+       line1: "123",
+      line2: "Fake Street",
+      city: "Manchester",
+      postcode: "M12 3AB"
+   }
+
+  return (
+     <div className="App">
+       <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+         <h1>Firstname: {address.firstname}</h1>
+        <h1>Surname: {address.surname}</h1>
+        <h1>Address: {address.line1}</h1>
+         <h1>{address.line2}</h1>
+        <h1>{address.city}</h1>
+         <h1>{address.postcode}</h1>
+        </header>
+
+        <a
+          className="App-link"
+           href="/app2.js"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           GO TO NEXT PAGE
+           </a>
+     </div>
+ );
+}
+
+
+export default AboutMe;

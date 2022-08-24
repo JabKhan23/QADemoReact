f17d3646ab5e296f8c1e13681b59eff951d7b07b
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-08-24 14:03:31 +0100

    added clock and messed around with lifecycle

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Header from './header.jsx';
 import Hello from './Hello.jsx';
 import './App.css';
 import ComponentWithProps from './Hello';
+import Clock from './Clock';
 
 const App = () => {
 return (
-    <ComponentWithProps/>
+    <><ComponentWithProps /><Clock /></>
 );
 }
 
